1b5e9879b3df2c398a79bd4ad9714ccbe86caebb
张宇翔 <1124751755@qq.com>
2016-12-22 08:59:39 +0800

    添加个人资料页面

--- app/src/main/java/note/neusoft/com/note/activity/PersonActivity.java
+++ app/src/main/java/note/neusoft/com/note/activity/PersonActivity.java
@@ -1,22 +1,175 @@
 package note.neusoft.com.note.activity;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.DisplayMetrics;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.PopupWindow;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import com.lidroid.xutils.ViewUtils;
+import com.lidroid.xutils.view.annotation.ViewInject;
+
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 import note.neusoft.com.note.R;
+import note.neusoft.com.note.utils.BmobConstants;
 
 public class PersonActivity extends Activity {
 
+    @ViewInject(R.id.img_back)
     private ImageButton img_back;
+    @ViewInject(R.id.mine_avatar)
     private CircleImageView mine_avatar;
+    @ViewInject(R.id.mine_nick)
+    private TextView mine_nick;
+    @ViewInject(R.id.edit)
+    private TextView edit;
+    @ViewInject(R.id.mine_sign_relative)
+    private RelativeLayout mine_sign_relative;
+    @ViewInject(R.id.sign_content)
+    private TextView sign_content;
+    @ViewInject(R.id.rl_account)
+    private RelativeLayout rl_account;
+    @ViewInject(R.id.account_content)
+    private TextView account_content;
+    @ViewInject(R.id.rl_nick)
+    private RelativeLayout rl_nick;
+    @ViewInject(R.id.tv_nick)
+    private TextView tv_nick;
+    @ViewInject(R.id.rl_sex)
+    private RelativeLayout rl_sex;
+    @ViewInject(R.id.tv_sex)
+    private TextView tv_sex;
+    @ViewInject(R.id.rl_email)
+    private RelativeLayout rl_email;
+    @ViewInject(R.id.tv_mail)
+    private TextView tv_mail;
+    @ViewInject(R.id.exit_btn)
+    private Button exit_btn;
+    private RelativeLayout layout_choose;
+    private RelativeLayout layout_photo;
+    private PopupWindow avatorPop;
+
+    protected int mScreenWidth;
+    protected int mScreenHeight;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_person);
         ViewUtils.inject(this);
+
+        DisplayMetrics metric = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(metric);
+        mScreenWidth = metric.widthPixels;
+        mScreenHeight = metric.heightPixels;
+
+        Init();
+    }
+
+    private void Init(){
+        img_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+
+        mine_avatar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showAvatarPop();
+            }
+        });
+    }
+
+    public String filePath = "";
+    private void showAvatarPop() {
+        View view = LayoutInflater.from(this).inflate(R.layout.pop_showavator,
+                null);
+        layout_choose = (RelativeLayout) view.findViewById(R.id.layout_choose);
+        layout_photo = (RelativeLayout) view.findViewById(R.id.layout_photo);
+        layout_photo.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+
+                // TODO Auto-generated method stub
+                layout_choose.setBackgroundColor(getResources().getColor(
+                        R.color.base_color_text_white));
+                layout_photo.setBackgroundDrawable(getResources().getDrawable(
+                        R.drawable.pop_bg_press));
+                File dir = new File(BmobConstants.MyAvatarDir);
+                if (!dir.exists()) {
+                    dir.mkdirs();
+                }
+                // 原图
+                File file = new File(dir, new SimpleDateFormat("yyMMddHHmmss")
+                        .format(new Date()));
+                filePath = file.getAbsolutePath();// 获取相片的保存路径
+                Uri imageUri = Uri.fromFile(file);
+
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+                startActivityForResult(intent,
+                        BmobConstants.REQUESTCODE_UPLOADAVATAR_CAMERA);
+            }
+        });
+        layout_choose.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+                // TODO Auto-generated method stub
+                //  ShowLog("点击相册");
+                layout_photo.setBackgroundColor(getResources().getColor(
+                        R.color.base_color_text_white));
+                layout_choose.setBackgroundDrawable(getResources().getDrawable(
+                        R.drawable.pop_bg_press));
+                Intent intent = new Intent(Intent.ACTION_PICK, null);
+                intent.setDataAndType(
+                        MediaStore.Images.Media.EXTERNAL_CONTENT_URI, "image/*");
+                startActivityForResult(intent,
+                        BmobConstants.REQUESTCODE_UPLOADAVATAR_LOCATION);
+            }
+        });
+
+        avatorPop = new PopupWindow(view, mScreenWidth, 600);
+        avatorPop.setTouchInterceptor(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if (event.getAction() == MotionEvent.ACTION_OUTSIDE) {
+                    avatorPop.dismiss();
+                    return true;
+                }
+                return false;
+            }
+        });
+
+        avatorPop.setWidth(WindowManager.LayoutParams.MATCH_PARENT);
+        avatorPop.setHeight(WindowManager.LayoutParams.WRAP_CONTENT);
+        avatorPop.setTouchable(true);
+        avatorPop.setFocusable(true);
+        avatorPop.setOutsideTouchable(true);
+        avatorPop.setBackgroundDrawable(new BitmapDrawable());
+        // 动画效果 从底部弹起
+        avatorPop.setAnimationStyle(R.style.Animations_GrowFromBottom);
+        avatorPop.showAtLocation(exit_btn, Gravity.BOTTOM, 0, 0);
     }
 }
